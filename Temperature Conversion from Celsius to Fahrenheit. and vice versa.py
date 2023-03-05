e5566a688ed3ef1d6dd3d94c054beec9899c49d6
Shubham Kadam <113159860+Shubhamkadam833@users.noreply.github.com>
2023-03-05 16:43:21 -0500

    Add files via upload

---
+++ Temperature Conversion from Celsius to Fahrenheit. and vice versa.py
@@ -0,0 +1,11 @@
+# Farenheight to celsius conversion
+Farenheight = input("Enter the temperature in Farenhight: ")
+Farenheight1 = int(Farenheight)
+celsius = (int(Farenheight) -32) * (5/9)
+print((celsius))
+
+# celsius to Farenheight conversion
+celsius = input("Enter the temperature in celsius: ")
+celsius1 = int(celsius)
+Farenheight = (int(celsius1) * (9/5)) + (32)
+print((Farenheight))
